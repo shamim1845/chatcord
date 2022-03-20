6bc9e66053665d8b568119d13ed8d3c177f0d4bc
shamim 1845m <samimraj1845@gmail.com>
2022-03-20 13:41:15 +0600

    change

--- server.js
+++ server.js
@@ -69,6 +69,6 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen( process.env.PORT5000 || 5000, () => {
+server.listen( process.env.PORT || 5000, () => {
   console.log("Server is running on port:", 5000);
 });
